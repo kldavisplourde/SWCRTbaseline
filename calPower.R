d82cc0ef6f1586cf32c3755ee239764a1d8cb0f8
Kendra Davis-Plourde <kendra.davis727@gmail.com>
2023-06-08 15:29:56 -0400

    Update calPower.R
    Instead of JM used BV

--- calPower.R
+++ calPower.R
@@ -70,9 +70,22 @@ calPower <- function(delta,vars,rho0,rho1,rho2,N,t,m,alpha=0.05,user.spec.df=0)
   sigma2.y0 <- vars[1]
   sigma2.y1 <- vars[2]
   
+  # Variance components
+  sigma2.s0 <- sigma2.y0*(rho0.0-rho1.0)
+  sigma2.s1 <- sigma2.y1*(rho0.1-rho1.1)
+  sigma.s01 <- sqrt(sigma2.y0)*sqrt(sigma2.y1)*(rho0.01-rho1.01)
+  
+  sigma2.b0 <- sigma2.y0*rho1.0
+  sigma2.b1 <- sigma2.y1*rho1.1
+  sigma.b01 <- sqrt(sigma2.y0)*sqrt(sigma2.y1)*rho1.01
+  
+  sigma2.e0 <- sigma2.y0 - sigma2.s0 - sigma2.b0
+  sigma2.e1 <- sigma2.y1 - sigma2.s1 - sigma2.b1
+  sigma.e01 <- sqrt(sigma2.y0)*sqrt(sigma2.y1)*(rho2.01-rho0.01)
+  
   # Variance of treatment effect: vard
     ## Bivariate model
-    vard.JM <- ((N*t/m)*sigma2.y1*(lambda2.0*lambda2.1-tau2^2)*(lambda3.0*lambda3.1-tau3^2))/((N*t*U-t*W+U^2-N*V)*lambda2.0*(lambda3.0*lambda3.1-tau3^2) - (U^2-N*V)*lambda3.0*(lambda2.0*lambda2.1-tau2^2))
+    vard.BV <- ((N*t/m)*sigma2.y1*(lambda2.0*lambda2.1-tau2^2)*(lambda3.0*lambda3.1-tau3^2))/((N*t*U-t*W+U^2-N*V)*lambda2.0*(lambda3.0*lambda3.1-tau3^2) - (U^2-N*V)*lambda3.0*(lambda2.0*lambda2.1-tau2^2))
   
     ## Change score model
     vard.CS <- ((N*t/m)*(sigma2.y1*lambda3.1 + sigma2.y0*lambda3.0 - 2*sqrt(sigma2.y0)*sqrt(sigma2.y1)*tau3)*(sigma2.y1*lambda2.1 + sigma2.y0*lambda2.0 - 2*sqrt(sigma2.y0)*sqrt(sigma2.y1)*tau2))/
@@ -82,26 +95,37 @@ calPower <- function(delta,vars,rho0,rho1,rho2,N,t,m,alpha=0.05,user.spec.df=0)
     vard.HG <- ((N*t/m)*sigma2.y1*lambda2.1*lambda3.1)/((N*t*U-t*W+U^2-N*V)*lambda3.1 - (U^2-N*V)*lambda2.1)
   
     ## ANCOVA model: Asymptotic variance
-    vard.ANCOVA <- ((N*t/m)*sigma2.y1*(lambda2.1-tau1^2/lambda1.0-2*sigma2.y0*tau1*(tau2-tau1)+sigma2.y0^2*tau1^2*(lambda2.0-lambda1.0))*(lambda3.1-tau1^2/lambda1.0-2*sigma2.y0*tau1*(tau3-tau1)+sigma2.y0^2*tau1^2*(lambda3.0-lambda1.0)))/
-      ((U^2+N*t*U-t*W-N*V)*(lambda3.1-2*sigma2.y0*tau1*tau3+sigma2.y0^2*tau1^2*lambda3.0)-(U^2-N*V)*(lambda2.1-2*sigma2.y0*tau1*tau2+sigma2.y0^2*tau1^2*lambda2.0)-(N*U-W)*t*(tau1^2/lambda1.0)*(sigma2.y0*lambda1.0-1)^2)
+    rho.01 <- sigma.e01/(sqrt(sigma2.e1)*sqrt(sigma2.e0))
+    gamma <- rho2.01*sqrt(sigma2.y1)/sqrt(sigma2.y0)
+    tilde.sigma2.s <- sigma2.s1 -2*gamma*sigma.s01 + gamma^2*sigma2.s0
+    tilde.sigma2.b <- sigma2.b1 -2*gamma*sigma.b01 + gamma^2*sigma2.b0
+    tilde.sigma2.e <- sigma2.e1*(1-rho.01^2)
+    num <- (N/m)*(tilde.sigma2.e+m*tilde.sigma2.s)*(tilde.sigma2.e+m*tilde.sigma2.s+t*m*tilde.sigma2.b)
+    den <- (N*U-W)*(tilde.sigma2.e+m*tilde.sigma2.s)+m*tilde.sigma2.b*(U^2+N*t*U-t*W-N*V)
+    vard.ANCOVA <- num/den
     
-    sigmaks.sq <- c(vard.HG,vard.JM,vard.CS,vard.ANCOVA)
+    #num<-(N/m)*(sigma2.y1*(lambda2.1-tau1^2/lambda1.0)-2*gamma*sqrt(sigma2.y1)*sqrt(sigma2.y0)*(tau2-tau1)+gamma^2*sigma2.y0*(lambda2.0-lambda1.0))*(sigma2.y1*(lambda3.1-tau1^2/lambda1.0)-2*gamma*sqrt(sigma2.y1)*sqrt(sigma2.y0)*(tau3-tau1)+gamma^2*sigma2.y0*(lambda3.0-lambda1.0))
+    #den<-(1/t)*(U^2+N*t*U-t*W-N*V)*(sigma2.y1*(lambda3.1-lambda2.1)-2*gamma*sqrt(sigma2.y1)*sqrt(sigma2.y0)*(tau3-tau2)+gamma^2*sigma2.y0*(lambda3.0-lambda2.0))+(N*U-W)*(sigma2.y1*(lambda2.1-tau1^2/lambda1.0)-2*gamma*sqrt(sigma2.y1)*sqrt(sigma2.y0)*(tau2-tau1)+gamma^2*sigma2.y0*(lambda2.0-lambda1.0))
+    #vard.ANCOVA <- num/den
+    #ABOVE EXPRESSION DOES NOT MATCH...
+    
+    sigmaks.sq <- c(vard.HG,vard.BV,vard.CS,vard.ANCOVA)
   
   #Power
   criticalValue.t.HG <- qt(p=(1-alpha), df=(N-2))
   criticalValue.t <- qt(p=(1-alpha), df=df.models)
   criticalValue.z <- qnorm(p=(1-alpha))
   pred.power.t.HG <- 1-pt(criticalValue.t, df=(N-2), delta/sqrt(vard.HG))
-  pred.power.t.JM <- 1-pt(criticalValue.t, df=df.models, delta/sqrt(vard.JM))
+  pred.power.t.BV <- 1-pt(criticalValue.t, df=df.models, delta/sqrt(vard.BV))
   pred.power.t.CS <- 1-pt(criticalValue.t, df=df.models, delta/sqrt(vard.CS))
   pred.power.t.ANCOVA <- 1-pt(criticalValue.t, df=df.models, delta/sqrt(vard.ANCOVA))
   pred.power.z.HG <- 1-pnorm(criticalValue.z, mean=(delta/sqrt(vard.HG)))
-  pred.power.z.JM <- 1-pnorm(criticalValue.z, mean=(delta/sqrt(vard.JM)))
+  pred.power.z.BV <- 1-pnorm(criticalValue.z, mean=(delta/sqrt(vard.BV)))
   pred.power.z.CS <- 1-pnorm(criticalValue.z, mean=(delta/sqrt(vard.CS)))
   pred.power.z.ANCOVA <- 1-pnorm(criticalValue.z, mean=(delta/sqrt(vard.ANCOVA)))
   
-  pred.power.t <- c(pred.power.t.HG,pred.power.t.JM,pred.power.t.CS,pred.power.t.ANCOVA)
-  pred.power.z <- c(pred.power.z.HG,pred.power.z.JM,pred.power.z.CS,pred.power.z.ANCOVA)
+  pred.power.t <- c(pred.power.t.HG,pred.power.t.BV,pred.power.t.CS,pred.power.t.ANCOVA)
+  pred.power.z <- c(pred.power.z.HG,pred.power.z.BV,pred.power.z.CS,pred.power.z.ANCOVA)
   
   names(sigmaks.sq)<-c("Follow-up Model (HG)","Bivariate Model","Change Score Model","ANCOVA Model")
   names(pred.power.t)<-c("Follow-up Model (HG)","Bivariate Model","Change Score Model","ANCOVA Model")
