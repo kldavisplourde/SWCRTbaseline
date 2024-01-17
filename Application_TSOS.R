b05e5591bcc58a8a4aa4c664e0740a94a530c868
Kendra Davis-Plourde <kendra.davis727@gmail.com>
2024-01-17 15:15:09 -0500

    Create Application_TSOS.R
    Application study

---
+++ Application_TSOS.R
@@ -0,0 +1,25 @@
+# Example Application of calPower.R using TSOS Study
+
+source("/Users/kdavis07/Documents/GitHub/SWCRTbaseline/calPower.R")
+
+#Inputs
+N <- 24     #Number of clusters
+t <- 5      #Number of periods
+m <- 8      #Number of observations within a cluster-period
+
+#Correlations
+rho2.01 <- 0.7            #Autocorrelation
+rho0.0 <- rho0.1 <- 0.02  #Assuming equal baseline- and endline-specific ICCs 
+rho0.01 <- 0.01           #Assuming between measure ICC is half of measure-specific ICC         
+rho1.0 <- rho1.1 <- 0.01  #Assuming CAC = 0.5
+rho1.01 <- 0.005          #Assuming between measure ICC is half of measure-specific ICC
+
+delta <- -0.23            #Intervention effect
+
+#Generate ICC matrices
+rho0<-matrix(c(rho0.0,rho0.01,rho0.01,rho0.1),2)
+rho1<-matrix(c(rho1.0,rho1.01,rho1.01,rho1.1),2)
+rho2<-matrix(c(1,rho2.01,rho2.01,1),2)
+
+#Generate power results
+calPower(delta=delta,vars=c(1,1),rho0=rho0,rho1=rho1,rho2=rho2,N=N,t=t,m=m,alpha=0.05,user.spec.df=N-2)
