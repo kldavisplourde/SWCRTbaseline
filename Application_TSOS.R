7b28c2f7d0b71c98ffa79c6b3d1a5ec1d6a22604
Kendra Davis-Plourde <kendra.davis727@gmail.com>
2024-05-10 15:25:34 -0400

    Update Application_TSOS.R

--- Application_TSOS.R
+++ Application_TSOS.R
@@ -13,8 +13,7 @@ rho0.0 <- rho0.1 <- 0.02  #Assuming equal baseline- and endline-specific ICCs
 rho0.01 <- 0.01           #Assuming between measure ICC is half of measure-specific ICC         
 rho1.0 <- rho1.1 <- 0.01  #Assuming CAC = 0.5
 rho1.01 <- 0.005          #Assuming between measure ICC is half of measure-specific ICC
-
-delta <- -0.23            #Intervention effect
+vars<-c(15^2,15^2)        #Total variance for baseline and endline outcomes - from protocol
 
 #Generate ICC matrices
 rho0<-matrix(c(rho0.0,rho0.01,rho0.01,rho0.1),2)
@@ -22,4 +21,7 @@ rho1<-matrix(c(rho1.0,rho1.01,rho1.01,rho1.1),2)
 rho2<-matrix(c(1,rho2.01,rho2.01,1),2)
 
 #Generate power results
-calPower(delta=delta,vars=c(1,1),rho0=rho0,rho1=rho1,rho2=rho2,N=N,t=t,m=m,alpha=0.05,user.spec.df=N-2)
+
+#ANCOVA with 85% power
+delta <- 0.23*sqrt(vars[1])          #Intervention effect
+calPower(delta=delta,vars=vars,rho0=rho0,rho1=rho1,rho2=rho2,N=N,t=t,m=m,alpha=0.05,user.spec.df=N-2)
